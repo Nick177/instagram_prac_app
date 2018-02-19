bb73df287e54ac951e0862218262f4412f41f7e5
Nicholas Rosas <nrosas@csumb.edu>
2018-02-19 15:30:01 -0800

    Initial Commit

---
+++ instagram_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram_app
+//
+//  Created by Nicholas Rosas on 2/19/18.
+//  Copyright © 2018 Nicholas Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
